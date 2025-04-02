c2c5abe9e9c0f448c2c41bf12af2da2ef687b584
Jeremy Oldfather <jeremy@chronulus.com>
2025-04-02 10:45:50 -0400

    Added auth token

--- .testing/config.sh
+++ .testing/config.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /tmp/pip
 
+CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --profile Artifacts --domain ${CA_DOMAIN_NAME} --domain-owner ${CA_AWS_ACCOUNT_ID}  --region ${AWS_REGION} --query authorizationToken --output text`
+
 echo "[global]
 index-url = https://aws:${CODEARTIFACT_AUTH_TOKEN}@${CA_DOMAIN_NAME}-${CA_AWS_ACCOUNT_ID}.d.codeartifact.${AWS_REGION}.amazonaws.com/pypi/${CA_REPOSITORY_NAME}/simple/
 extra-index-url = https://pypi.org/simple/
